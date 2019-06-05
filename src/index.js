854da3e546f565592063bb2e495c18d6cda0551d
Alon Greyber <alongreyber@gmail.com>
2019-06-05 19:43:28 -0400

    Working on SVG conversion

--- src/index.js
+++ src/index.js
@@ -1,59 +1,35 @@
-import _ from 'lodash';
-import * as THREE from 'three';
 import { STLLoader } from 'three/examples/jsm/loaders/STLLoader.js';
-import { SVGRenderer } from 'three/examples/jsm/renderers/SVGRenderer.js';
+var stlLoader = new STLLoader();
 
-var loader = new STLLoader();
+import { STLToSVG } from './stl-processor.js';
 
-loader.load("example-stl/elbow.stl", function (geometry) {
-    // create a scene, that will hold all our elements such as objects, cameras and lights.
-    var scene = new THREE.Scene();
-    // add spotlight for the shadows
-    var spotLight = new THREE.SpotLight(0xffffff);
-    spotLight.position.set(150, 150, 150);
-    scene.add(spotLight);
-    // create a render and set the size
-    var webGLRenderer = new THREE.WebGLRenderer();
-    webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
-    webGLRenderer.setSize(window.innerWidth, window.innerHeight);
-    webGLRenderer.shadowMapEnabled = true;
-    // add the output of the renderer to the html element
-    document.getElementById("WebGL-output").appendChild(webGLRenderer.domElement);
-    // call the render function
-    var step = 0;
-    // model from http://www.thingiverse.com/thing:69709
-    var group = new THREE.Object3D();
-    var camera;
-    geometry.center();
-    var mat = new THREE.MeshBasicMaterial({color: 0x7777ff});
-    group = new THREE.Mesh(geometry, mat);
-    var bbox = new THREE.Box3().setFromObject(group);
-    console.log('Bounding Box:');
-    console.log(bbox);
-    //group.rotation.x = -0.5 * Math.PI;
-    group.scale.set(0.6, 0.6, 0.6);
-    scene.add(group);
+document.addEventListener("DOMContentLoaded", function(event) { 
+    var partsInputElement = document.getElementById('parts-input');
+    partsInputElement.addEventListener('change', function() {
+	var partsListElement = document.getElementById('parts-list');
+	partsListElement.innerHTML = '';
+	Array.from(this.files).forEach(file => {
+	    var rowTemplate = document.getElementById('part-row');
+	    var clone = document.importNode(rowTemplate.content, true);
+	    var td = clone.querySelectorAll("td");
+	    td[0].textContent = file.name;
 
-    // create a camera, which defines where we're looking at.
-    camera = new THREE.OrthographicCamera( bbox.min.x, bbox.max.x, bbox.max.z, bbox.min.z, 1, 500);
-    // position and point the camera to the center of the scene
-    camera.position.x = 0;
-    camera.position.y = 50;
-    camera.position.z = 0;
-    camera.lookAt(new THREE.Vector3(0, 0, 0));
+	    partsListElement.appendChild(clone);
+	});
+    });
 
-    var svgRenderer = new SVGRenderer();
-    svgRenderer.setClearColor(0xffffff);
-    svgRenderer.setSize(window.innerWidth, window.innerHeight);
-    svgRenderer.setQuality('high');
-    svgRenderer.render(scene,camera);
-    console.log('SVG: ');
-    console.log(svgRenderer.domElement);
-
-    function render() {
-	// render using requestAnimationFrame
-	requestAnimationFrame(render);
-	webGLRenderer.render(scene, camera);
-    }
-    render();
+    var loadSTLButton = document.getElementById('load-stl');
+    loadSTLButton.addEventListener('click', function() {
+	var processFileFunc = function(event) {
+	    var url = event.target.result;
+	    stlLoader.load(url, STLToSVG);
+	};
+	// Read each file
+	Array.from(partsInputElement.files).forEach(file => {
+	    // To access file from a form we are using the HTLM5 file API
+	    var fileReader = new FileReader();
+	    fileReader.onload = processFileFunc;
+	    fileReader.readAsDataURL(file);
+	});
+    });
 });
---
+++ src/stl-processor.js
@@ -0,0 +1,70 @@
+
+import * as THREE from 'three';
+import { SVGRenderer } from 'three/examples/jsm/renderers/SVGRenderer.js';
+
+import { ImageTracer } from './imagetracer.js';
+
+export function STLToSVG(geometry) {
+    // create a scene, that will hold all our elements such as objects, cameras and lights.
+    var scene = new THREE.Scene();
+    // add spotlight for the shadows
+    var spotLight = new THREE.SpotLight(0xffffff);
+    spotLight.position.set(150, 150, 150);
+    scene.add(spotLight);
+    // create a render and set the size
+    var webGLRenderer = new THREE.WebGLRenderer({ preserveDrawingBuffer: true });
+    webGLRenderer.setClearColor(new THREE.Color(0x000, 1.0));
+    webGLRenderer.setSize(window.innerWidth, window.innerHeight);
+    webGLRenderer.shadowMapEnabled = true;
+    // add the output of the renderer to the html element
+    document.getElementById("WebGL-output").appendChild(webGLRenderer.domElement);
+    // call the render function
+    var step = 0;
+    // model from http://www.thingiverse.com/thing:69709
+    var group = new THREE.Object3D();
+    var camera;
+    geometry.center();
+    var mat = new THREE.MeshBasicMaterial({color: 0x7777ff});
+    mat.wireframe = true;
+    group = new THREE.Mesh(geometry, mat);
+    var bbox = new THREE.Box3().setFromObject(group);
+    console.log('Bounding Box:');
+    console.log(bbox);
+    //group.rotation.x = -0.5 * Math.PI;
+    group.scale.set(0.6, 0.6, 0.6);
+    scene.add(group);
+
+    // create a camera, which defines where we're looking at.
+    camera = new THREE.OrthographicCamera( bbox.min.x, bbox.max.x, bbox.max.z, bbox.min.z, 1, 500);
+    // position and point the camera to the center of the scene
+    camera.position.x = 0;
+    camera.position.y = 50;
+    camera.position.z = 0;
+    camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    /*
+    var svgRenderer = new SVGRenderer();
+    svgRenderer.setClearColor(0xffffff);
+    svgRenderer.setSize(window.innerWidth, window.innerHeight);
+    svgRenderer.setQuality('high');
+    svgRenderer.render(scene,camera);
+    console.log('SVG: ');
+    console.log(svgRenderer.domElement);
+    */
+
+    // Convert to SVG
+    var pngDataURL = webGLRenderer.domElement.toDataURL();
+    console.log(pngDataURL);
+    ImageTracer.imageToSVG(pngDataURL, function(svgString) {
+	ImageTracer.appendSVGString( svgString, 'svgContainer');
+
+    }, 'posterized2d');
+
+    // render using requestAnimationFrame
+    function render() {
+	requestAnimationFrame(render);
+	webGLRenderer.render(scene, camera);
+    }
+    render();
+
+};
